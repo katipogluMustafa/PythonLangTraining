35e1fd0f9e6c41b2591252317af16b4ba5b9141f
Mustafa <mustafakatipoglu98@hotmail.com>
2019-09-11 14:54:21 +0300

    Output Formatting

---
+++ 03_CodeSnippets/00_Language_Fundamentals/01_Basics/06_Output_Formatting.py
@@ -0,0 +1,19 @@
+
+print(1,2,3,4,5,sep="--")
+
+print(1,2,3,4,sep = "-_-", end="***")
+
+print(1,2,3,4,sep = "-_-", end="***", flush=True)       # Flush the output buffer
+
+x = 5
+y = 8
+
+print("\nThe value of x is {} and y is {}".format(x,y))
+
+print("\nThe value of x is {1} and y is {0}".format(x,y))
+
+print("\nI liked {first} but not the same way with {second}".format(first="Python", second="C++"))
+
+print("The value of x is %f" %1.5)          # sprintf() syntax styling
+print("The value of x is %d" %1)
+
