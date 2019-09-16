61758942727cdcb5784cdebaf9097cacb606f744
Mustafa <mustafakatipoglu98@hotmail.com>
2019-09-16 17:29:38 +0300

    Operator Overloading

---
+++ 03_CodeSnippets/00_Language_Fundamentals/06_OOP/03_Operator_Overloading.py
@@ -0,0 +1,55 @@
+# Operator Overloading
+
+class Point:
+    def __init__(self, x = 0, y = 0):
+        self.x = x
+        self.y = y
+
+    def __str__(self):              # overloading char const* conversion operator(with C++ terms)
+        return "({0},{1})".format(self.x, self.y)
+
+    def __add__(self, other):       # overloading + operator
+        x = self.x + other.x
+        y = self.y + other.y
+        return Point(x,y)           # you need to return back the same class
+
+    def __lt__(self, other):
+        self_mag  = (self.x ** 2) + ( self.y **2)
+        other_mag = (other.x ** 2) + ( other.y **2 )
+        return self_mag < other_mag
+
+
+print( Point(2,3) )
+print( Point(2,3) + Point(5,7) )
+
+# What actually happens is that, when you do p1 + p2,
+# Python will call p1.__add__(p2) which in turn is Point.__add__(p1,p2).
+
+print( Point(2,3) < Point(-2,5) )           # Prints True
+print( Point(2,7) < Point(-2,5) )           # Prints False
+
+
+            # Arithmetic Operators
+# Operator	           Expression	      Internally
+# Addition	            p1 + p2	        p1.__add__(p2)
+# Subtraction	        p1 - p2	        p1.__sub__(p2)
+# Multiplication	    p1 * p2	        p1.__mul__(p2)
+# Power	                p1 ** p2	    p1.__pow__(p2)
+# Division	            p1 / p2	        p1.__truediv__(p2)
+# Floor Division	    p1 // p2    	p1.__floordiv__(p2)
+# Remainder (modulo)	p1 % p2	        p1.__mod__(p2)
+# Bitwise Left Shift	p1 << p2	    p1.__lshift__(p2)
+# Bitwise Right Shift	p1 >> p2	    p1.__rshift__(p2)
+# Bitwise AND	        p1 & p2	        p1.__and__(p2)
+# Bitwise OR	        p1 | p2	        p1.__or__(p2)
+# Bitwise XOR	        p1 ^ p2	        p1.__xor__(p2)
+# Bitwise NOT	        ~p1	            p1.__invert__()
+
+                 # Comparison Operators
+# Operator	               Expression	      Internally
+# Less than	                p1 < p2	        p1.__lt__(p2)
+# Less than or equal to	    p1 <= p2	    p1.__le__(p2)
+# Equal to                  p1 == p2	    p1.__eq__(p2)
+# Not equal to	            p1 != p2	    p1.__ne__(p2)
+# Greater than	            p1 > p2	        p1.__gt__(p2)
+# Greater than or equal to	p1 >= p2	    p1.__ge__(p2)
